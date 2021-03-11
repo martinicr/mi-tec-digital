655ffc9cb02b0c6fcddf0877835a3b4b88a180b3
Martin Flores <martin.flores@bodybuilding.com>
2021-03-10 23:20:07 -0600

    codigo inicial para tarea 2

--- src/main/java/tec/bd/app/service/EstudianteService.java
+++ src/main/java/tec/bd/app/service/EstudianteService.java
@@ -1,43 +1,24 @@
 package tec.bd.app.service;
 
-import tec.bd.app.bd.SetDB;
 import tec.bd.app.domain.Estudiante;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
-public class EstudianteService {
+public interface EstudianteService {
 
-    private SetDB database;
+    List<Estudiante> getAll();
 
-    public EstudianteService(SetDB database) {
-        this.database = database;
-    }
+    Optional<Estudiante> getById(int carne);
 
-    public List<Estudiante> getAll() {
-        return this.database.getEstudianteTable()
-                .stream()
-                .collect(Collectors.toList());
-    }
+    void addNew(Estudiante e);
 
-    public Estudiante getById(long carne) {
-        return this.database.getEstudianteTable().stream().filter(e -> e.getCarne() == carne).findFirst().get();
-    }
+    Optional<Estudiante> updateStudent(Estudiante e);
 
-    public void addNew(Estudiante e) {
-        // Verificar si el estudiante que viene por parametro ya existe en la BD
-        this.database.getEstudianteTable().add(e);
-    }
+    void deleteStudent(int carne);
 
-    public void updateStudent(Estudiante e) {
-        // Implementar codigo de actualizacion
-        // Traer el estudiante con carne X
-        // modificar el estudiante con los datos del estudiante e
-    }
+    List<Estudiante> getStudentsSortedByLastName();
 
-    public void deleteStudent(long carne) {
-        // implementar codigo de borrado
-        // HashSet.remove(e);
-    }
+    List<Estudiante> getStudentsByLastName(String lastName);
 
 }
